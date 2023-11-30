f8b0102a822140bb121221b944f4f34796a3e603
BeyondMental <35311433+BeyondMental@users.noreply.github.com>
2023-11-30 15:58:45 -0500

    Create expandImage.js

---
+++ expandImage.js
@@ -0,0 +1,10 @@
+function getPics() {}
+const imgs = document.querySelectorAll('#about-me-section img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
