c00ad90134718e67f7371f21bf2321991771ebd0
BeyondMental <35311433+BeyondMental@users.noreply.github.com>
2023-12-02 17:15:03 -0500

    Update expandImage.js

--- expandImage.js
+++ expandImage.js
@@ -1,10 +1,1 @@
-function getPics() {}
-const imgs = document.querySelectorAll('#about-me-section img');
-const fullPage = document.querySelector('#fullpage');
 
-imgs.forEach(img => {
-  img.addEventListener('click', function() {
-    fullPage.style.backgroundImage = 'url(' + img.src + ')';
-    fullPage.style.display = 'block';
-  });
-});
